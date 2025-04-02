09e919d78c0263cb46b6b7474cf494b2093f52a8
Tresor <tresorkabis@gmail.com>
2025-04-02 19:24:01 +0100

    auth init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { Refine } from "@refinedev/core";
+import { Refine, Authenticated } from "@refinedev/core";
 
 import { dataProvider } from "./providers/data-provider";
+import { authProvider } from "./providers/auth-provider";
+
 import { ShowProduct } from "./pages/products/show";
 import { EditProduct } from "./pages/products/edit";
 import { ListProducts } from "./pages/products/list";
 import { CreateProduct } from "./pages/products/create";
 
+import { Login } from "./pages/home/login";
+import { Header } from "./components/header";
+
 function App() {
   return (
-    <Refine dataProvider={dataProvider}>
-      {/* <ShowProduct /> */}
-      {/* <EditProduct />  */}
-      <ListProducts />
-      {/* <CreateProduct /> */}
+    <Refine dataProvider={dataProvider} authProvider={authProvider}>
+      <Authenticated 
+        key="protected" 
+        fallback={<Login />}
+      >
+        <Header />
+        {/* <ShowProduct /> */}
+        {/* <EditProduct />  */}
+        <ListProducts />
+        {/* <CreateProduct /> */}
+      </Authenticated>
     </Refine>
   );
 }
--- src/providers/auth-provider.ts
+++ src/providers/auth-provider.ts
@@ -1,14 +1,62 @@
 import { AuthProvider } from "@refinedev/core";
 
 export const authProvider: AuthProvider = {
-    login: async ({ email, password }) => { throw new Error("Not implemented"); },
-    logout: async () => { throw new Error("Not implemented"); },
-    check: async () => { throw new Error("Not implemented"); },
+    login: async ({ email, password }) => { 
+        const response = await fetch(
+            "https://api.fake-rest.refine.dev/auth/login",
+            {
+              method: "POST",
+              body: JSON.stringify({ email, password }),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            },
+          );
+
+          const data = await response.json();
+
+          if (data.token) {
+            localStorage.setItem("my_access_token", data.token);
+            return { success: true};
+          }
+
+          return { success:false };
+    },
+
+    logout: async () => { 
+        localStorage.removeItem("my_access_token");
+
+        return { success: true }
+     },
+    
+    check: async () => { 
+        const token = localStorage.getItem("my_access_token");
+        return { authenticated: Boolean(token) };
+     },
+    
     onError: async (error) => { throw new Error("Not implemented"); },
     // optional methods
     register: async (params) => { throw new Error("Not implemented"); },
+    
     forgotPassword: async (params) => { throw new Error("Not implemented"); },
+    
     updatePassword: async (params) => { throw new Error("Not implemented"); },
-    getIdentity: async () => { throw new Error("Not implemented"); },
+    
+    getIdentity: async () => {
+      const response = await fetch("https://api.fake-rest.refine.dev/auth/me", {
+        headers: {
+          Authorization: localStorage.getItem("my_access_token") || "",
+        },
+      });
+  
+      if (response.status < 200 || response.status > 299) {
+        return null;
+      }
+  
+      const data = await response.json();
+  
+      return data;
+    },
+    
     getPermissions: async () => { throw new Error("Not implemented"); },
 };
